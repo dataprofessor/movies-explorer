6c0904dd3f7cfa78901e2c9097c61ff5d07bd719
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-12-15 19:03:32 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,8 @@ year_list = df.year.unique()
 year_selection = st.slider('Select year duration', 1986, 2006, (2001, 2006))
 # year_selection = st.slider(int(year_list.min()), int(year_list.max()), (int(year_list[-10]), int(year_list[-1])))
 
+st.header()
+
 # Display DataFrame
 df_selection
 
