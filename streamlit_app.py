3f2c8bc18c1940a3930ec5d777152129ca811657
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-12-15 10:49:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,18 @@
 import streamlit as st
+import pandas as pd
 
-st.title('🎈 App Name')
+st.title('📊 Exploratory Data Analysis')
 
-st.write('Hello world!')
+st.info('This demo shows the use of Pandas for loading data as DataFrames and Altair for chart creation.')
+
+st.header('Which movie Genre performs best at the box office?')
+
+# Load data
+df = pd.read_csv('data/tmdb_5000_movies_mergedwith_movie_metadata.csv')
+
+genres_list = ['Action', 'Adventure', 'Animation', 'Biography', 'Comedy', 'Crime', 
+               'Documentary', 'Drama', 'Family', 'Fantasy', 'Film-Noir', 'History', 
+               'Horror', 'Music', 'Musical', 'Mystery', 'News', 'Romance', 
+               'Sci-Fi', 'Short', 'Sport', 'Thriller', 'War', 'Western']
+genres_selection = st.multiselect('Select genres', genres_list, 'Action')
+st.write(genres_selection)
