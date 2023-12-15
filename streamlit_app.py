0d2452ae846d0d94e7629fd66072a0381ce292e5
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-12-15 19:10:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ year_selection = st.slider('Select year duration', 1986, 2006, (2001, 2006))
 
 # Display DataFrame
 with placeholder1:
-    df_selection
+    df_editor = st.data_editor(df_selection)
 
 # Display chart
 with placeholder2:
