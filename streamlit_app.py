dff3f59b3310bb98f96253432d77f2406ebb2cd2
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-12-15 19:40:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ year_selection_list = list(np.arange(year_selection[0], year_selection[1]+1))
 
 # df.genre.isin(genres_selection)
 # df.year.isin(year_selection_list)
-df_selection = df[ df.genre.isin(genres_selection) ]
+df_selection = df[ df.year>2002 ]
 
 df.year
 
