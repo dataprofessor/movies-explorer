8448fce1f5a453be8b8ff4158fa863875f2012a1
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-12-15 14:47:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title('📊 Exploratory Data Analysis')
 
 st.info('This demo shows the use of Pandas for loading data as DataFrames and Altair for chart creation.')
 
-st.header('Which movie Genre performs best at the box office?')
+st.header('Which Movie Genre performs best at the box office?')
 
 # Load data
 df = pd.read_csv('data/movies_genres_summary.csv')
