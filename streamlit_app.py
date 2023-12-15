a3fcad2d49f1d6e6e06ce7da32f1391d49f039e8
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-12-15 14:41:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,11 +8,12 @@ st.info('This demo shows the use of Pandas for loading data as DataFrames and Al
 st.header('Which movie Genre performs best at the box office?')
 
 # Load data
-df = pd.read_csv('data/tmdb_5000_movies_mergedwith_movie_metadata.csv')
+df = pd.read_csv('data/movie_genres_summary.csv')
 
-genres_list = ['Action', 'Adventure', 'Animation', 'Biography', 'Comedy', 'Crime', 
-               'Documentary', 'Drama', 'Family', 'Fantasy', 'Film-Noir', 'History', 
-               'Horror', 'Music', 'Musical', 'Mystery', 'News', 'Romance', 
-               'Sci-Fi', 'Short', 'Sport', 'Thriller', 'War', 'Western']
-genres_selection = st.multiselect('Select genres', genres_list, 'Action')
-st.write(genres_selection)
+genres_list = df.genre.unique()
+
+# genres_list 
+genres_selection = st.multiselect('Select genres', genres_list, ['Action', 'Adventure', 'Biography', 'Comedy', 'Drama', 'Horror'])
+df_selection = df_genres[df_genres.genre.isin(genres_selection)]
+
+df_selection
