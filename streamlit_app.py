5f24a9a08fcbaafd90d757ef52c6a622487f77b0
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-12-16 16:22:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,12 @@ import numpy as np
 import pandas as pd
 import altair as alt
 
+# Page title
 st.set_page_config(
     page_title="Exploratory Data Analysis",
     page_icon="📊")
+st.title('📊 Exploratory Data Analysis')
+st.info('This demo shows the use of Pandas for loading data as DataFrames and Altair for chart creation.')
 
 # CSS styling
 st.markdown("""
@@ -18,10 +21,6 @@ st.markdown("""
 
 </style>
 """, unsafe_allow_html=True)
-
-st.title('📊 Exploratory Data Analysis')
-
-st.info('This demo shows the use of Pandas for loading data as DataFrames and Altair for chart creation.')
 
 st.subheader('Which Movie Genre performs best ($) at the box office?')
 
@@ -42,35 +41,16 @@ df_selection = df[df.genre.isin(genres_selection)] # & df['year'].isin(year_sele
 reshaped_df = df_selection.pivot_table(index='year', columns='genre', values='gross', aggfunc='sum', fill_value=0)
 reshaped_df = reshaped_df.sort_values(by='year', ascending=False)
 
-# Create a Selection boolean list
-#num_rows = len(reshaped_df)
-#boolean_list = [True] * min(5, num_rows) + [False] * max(0, num_rows - 5)
-#reshaped_df.insert(0, 'selection', pd.Series(boolean_list))
-
-# Placeholder
-placeholder1 = st.empty()
-placeholder2 = st.empty()
-
 # Display DataFrame
-with placeholder1:
-    df_editor = st.data_editor(reshaped_df, height=212, use_container_width=True,
+df_editor = st.data_editor(reshaped_df, height=212, use_container_width=True,
                                column_config={"year": st.column_config.TextColumn("Year")},
                                num_rows="dynamic")
-
-    ## Year selection
-    #year_list = df_editor.Year.unique()
-    #year_selection_list = list(np.arange(year_list[0], year_list[1]+1))
-    #df_editor_selection = df_editor[df_editor['Year'].isin(year_selection_list)]
-
-
 df_chart = pd.melt(df_editor.reset_index(), id_vars='year', var_name='genre', value_name='gross')
 
 # Display chart
-with placeholder2:
-    chart = alt.Chart(df_chart).mark_line().encode(
+chart = alt.Chart(df_chart).mark_line().encode(
         x=alt.X('year:N', title='Year'),
         y=alt.Y('gross:Q', title='Gross earnings ($)'),
         color='genre:N'
-    )
-    st.altair_chart(chart, use_container_width=True)
-
+        )
+st.altair_chart(chart, use_container_width=True)
