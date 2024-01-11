4ca2039d040316390dbcabdc79717f4c4b870c99
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-01-11 08:57:39 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import altair as alt
 
 # Page title
-st.set_page_config(page_title='Exploratory Data Analysis', page_icon='📊', layout='wide')
+st.set_page_config(page_title='Exploratory Data Analysis', page_icon='📊')
 st.title('📊 Exploratory Data Analysis')
 st.info('This app shows the use of Pandas for data wrangling, Altair for chart creation and editable dataframe for data interaction.')
 
