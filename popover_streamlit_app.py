b0b3b92b779bc28dc4be03b4549ac753c7c5fc4d
sfc-gh-eberger <142522520+sfc-gh-eberger@users.noreply.github.com>
2024-03-07 13:06:05 -0800

    Update popover_streamlit_app.py

--- popover_streamlit_app.py
+++ popover_streamlit_app.py
@@ -54,7 +54,7 @@ with st.popover('Want to learn more ask our chatbot'):
   st.markdown("**Chat with the data**")
   # Initialize chat history
   if "messages" not in st.session_state:
-    st.session_state.messages = ["What is the highest grossing movie category"]
+    st.session_state.messages = []
   # Display chat messages from history on app rerun
   for message in st.session_state.messages:
     with st.chat_message(message["role"]):
