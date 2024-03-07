32f28081b9394e756ff3c9be96eeda6df8c3f6ae
sfc-gh-eberger <142522520+sfc-gh-eberger@users.noreply.github.com>
2024-03-07 14:27:22 -0800

    Update popover_streamlit_app.py

--- popover_streamlit_app.py
+++ popover_streamlit_app.py
@@ -71,26 +71,3 @@ with st.popover('Chat with the data'):
             {"role": "assistant", "content": f"Echo: {prompt}"}
         )
 
-with st.popover('Chat with the data'):
-  st.markdown("**Find additional insights through chat**")
-  st.markdown("##")
-  # Initialize chat history
-  if "messages" not in st.session_state:
-    st.session_state.messages = []
-  # Display chat messages from history on app rerun
-  for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-    with chat_container:
-    # React to user input
-    if prompt := st.chat_input("Ask me about the data"):
-      with chat_container:
-        # Display user message in chat message container
-        st.chat_message("user").markdown(prompt)
-        # Add user message to chat history
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        response = f"Adventure is the highest grossing category"
-        # Display assistant response in chat message container
-        st.markdown(response)
-        # Add assistant response to chat history
-        st.session_state.messages.append({"role": "assistant", "content": response})
