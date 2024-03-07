307a6b4feade90b8c13fb3d80799be061040d3e6
sfc-gh-eberger <142522520+sfc-gh-eberger@users.noreply.github.com>
2024-03-07 10:50:30 -0800

    Update popover_streamlit_app.py

--- popover_streamlit_app.py
+++ popover_streamlit_app.py
@@ -11,7 +11,6 @@ with st.popover('About this app'):
   st.markdown('**What can this app do?**')
   st.info('This app shows the use of Pandas for data wrangling, Altair for chart creation and editable dataframe for data interaction.')
   st.markdown('**How to use the app?**')
-  st.warning('To engage with the app, 1. Select genres of your interest in the drop-down selection box and then 2. Select the year duration from the slider widget. As a result, this should generate
   
 st.subheader('Which Movie Genre performs ($) best at the box office?')
 
