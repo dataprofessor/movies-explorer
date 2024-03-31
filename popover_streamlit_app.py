ff4b922f81b8fdce463a69958ec8736cd871a52c
sfc-gh-eberger <142522520+sfc-gh-eberger@users.noreply.github.com>
2024-03-30 21:53:18 -0700

    Update popover_streamlit_app.py

--- popover_streamlit_app.py
+++ popover_streamlit_app.py
@@ -50,25 +50,3 @@ chart = alt.Chart(df_chart).mark_line().encode(
             ).properties(height=320)
 st.altair_chart(chart, use_container_width=True)
 
-with st.popover('Chat with the data'):
-  st.markdown("**Find additional insights**")
-  st.markdown("##")
-  # Initialize chat history
-  if "messages" not in st.session_state:
-    st.session_state.messages = []
-  # Display chat messages from history on app rerun
-  for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-  # React to user input
-  if prompt := st.chat_input("Ask data questions here?"):
-    # Display user message in chat message container
-    st.chat_message("user").markdown(prompt)
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    response = f"Adventure is the highest grossing category"
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        st.markdown(response)
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
